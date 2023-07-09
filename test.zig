244c8eadeca53c9fab247e8807568d6cff09bfcb
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-07-09 07:46:50 -0500

    updated

--- test.zig
+++ test.zig
@@ -55,3 +55,20 @@ test "while with break" {
     }
     try expect(sum == 1);
 }
+
+test "while" {
+  var i: u8 = 2;
+  while (i < 100) {
+    i *= 2;
+  }
+  try expect(i == 128);
+}
+
+test "while with continue expression"{
+  var sum: u8 = 0;
+  var i: u8 = 1;
+  while (i <= 10) : (i += 1) {
+    sum +=i;
+  }
+  try expect(sum == 55);
+}
